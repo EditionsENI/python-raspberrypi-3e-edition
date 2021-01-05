d887e4b22dd6e9f708f6e9f2d4400f4a7c401ea3
Patrice Clement <monsieurp@gentoo.org>
2021-01-06 00:11:22 +0100

    comprehension_1.py: code refactoring

--- Chapitre_2/comprehension_1.py
+++ Chapitre_2/comprehension_1.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
-ma_liste = list(range(1, 11))
-mon_tri = []
 
-for i in ma_liste:
-    if i % 2 == 0:
-        mon_tri.append(i)
+def main():
+    ma_liste = list(range(1, 11))
+    mon_tri = []
 
-print(mon_tri)
+    for i in ma_liste:
+        if i % 2 == 0:
+            mon_tri.append(i)
+
+    print(mon_tri)
+
+
+if __name__ == "__main__":
+    main()
