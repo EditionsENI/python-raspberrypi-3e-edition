f3c35c412805bf085050cb3a4687a71b245fffec
Patrice Clement <monsieurp@gentoo.org>
2021-01-19 23:17:17 +0100

    Chapitre_7: mise a jour des scripts.

--- Chapitre_7/enregistreur/boutons.py
+++ Chapitre_7/enregistreur/boutons.py
@@ -6,28 +6,28 @@ import os
 
 
 def main():
-    if not os.path.exists('resources'):
-        os.mkdir('resources')
+    if not os.path.exists("resources"):
+        os.mkdir("resources")
 
-    img = Image.new('RGBA', (100, 100))
+    img = Image.new("RGBA", (100, 100))
     draw = ImageDraw.Draw(img)
-    draw.ellipse((25, 25, 75, 75), fill='red')
+    draw.ellipse((25, 25, 75, 75), fill="red")
     img = img.resize((25, 25))
-    img.save('resources/enreg.png')
+    img.save("resources/enreg.png")
 
-    img = Image.new('RGBA', (100, 100))
+    img = Image.new("RGBA", (100, 100))
     draw = ImageDraw.Draw(img)
-    draw.polygon([(25, 50), (75, 25), (75, 75)], fill='black')
+    draw.polygon([(25, 50), (75, 25), (75, 75)], fill="black")
     img = ImageOps.mirror(img)
     img = img.resize((25, 25))
-    img.save('resources/lire.png')
+    img.save("resources/lire.png")
 
-    img = Image.new('RGBA', (100, 100))
+    img = Image.new("RGBA", (100, 100))
     draw = ImageDraw.Draw(img)
-    draw.polygon([(25, 25), (75, 25), (75, 75), (25, 75)], fill='black')
+    draw.polygon([(25, 25), (75, 25), (75, 75), (25, 75)], fill="black")
     img = img.resize((25, 25))
-    img.save('resources/stop.png')
+    img.save("resources/stop.png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- Chapitre_7/enregistreur/enregistreur.py
+++ Chapitre_7/enregistreur/enregistreur.py
@@ -11,10 +11,10 @@ import threading
 class PiAudio(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.nom_carte = 'sysdefault:CARD=U0x46d0x825'
+        self.nom_carte = "sysdefault:CARD=U0x46d0x825"
         self.fichier_courant = None
         self.stop_thread = False
-        self.options_fichier = {'filetypes': [('Fichiers .wav', '.wav')]}
+        self.options_fichier = {"filetypes": [("Fichiers .wav", ".wav")]}
         self.creer_composant()
         self.mainloop()
 
@@ -26,48 +26,48 @@ class PiAudio(Tk):
         self.frame_bas = Frame(self)
         self.frame_bas.grid(row=1)
         self.bouton_ouvrir = Button(self.frame_haut,
-                                    text='Lire\nun enregistrement',
+                                    text="Lire\nun enregistrement",
                                     command=lambda: self.ouvrir())
         self.bouton_ouvrir.grid(row=0, column=0)
         self.bouton_enreg_sous = Button(self.frame_haut,
-                                        text='Enregistrer\ndu son',
+                                        text="Enregistrer\ndu son",
                                         command=lambda: self.enregistrer_sous())
         self.bouton_enreg_sous.grid(row=0, column=1)
-        self.lire_image = PhotoImage(file='resources/lire.png'),
+        self.lire_image = PhotoImage(file="resources/lire.png"),
         self.bouton_lire = Button(self.frame_bas,
                                   image=self.lire_image,
                                   command=lambda: self.lire())
         self.bouton_lire.grid(row=1, column=0)
-        self.bouton_lire.configure(state='disabled')
-        self.stop_image = PhotoImage(file='resources/stop.png'),
+        self.bouton_lire.configure(state="disabled")
+        self.stop_image = PhotoImage(file="resources/stop.png"),
         self.bouton_stop = Button(self.frame_bas,
                                   image=self.stop_image,
                                   command=lambda: self.stop())
         self.bouton_stop.grid(row=1, column=1)
-        self.bouton_stop.configure(state='disabled')
-        self.enreg_image = PhotoImage(file='resources/enreg.png'),
+        self.bouton_stop.configure(state="disabled")
+        self.enreg_image = PhotoImage(file="resources/enreg.png"),
         self.bouton_enreg = Button(self.frame_bas,
                                    image=self.enreg_image,
                                    command=lambda: self.enregistrer())
         self.bouton_enreg.grid(row=1, column=2)
-        self.bouton_enreg.configure(state='disabled')
+        self.bouton_enreg.configure(state="disabled")
         self.title(self.__class__.__name__)
 
     def ouvrir(self):
         fichier = askopenfilename(**self.options_fichier)
         if fichier:
             self.fichier_courant = fichier
-            self.bouton_lire.configure(state='active')
-            self.bouton_stop.configure(state='active')
-            self.bouton_enreg.configure(state='disabled')
+            self.bouton_lire.configure(state="active")
+            self.bouton_stop.configure(state="active")
+            self.bouton_enreg.configure(state="disabled")
 
     def enregistrer_sous(self):
         fichier = asksaveasfilename(**self.options_fichier)
         if fichier:
             self.fichier_courant = fichier
-            self.bouton_lire.configure(state='disabled')
-            self.bouton_stop.configure(state='active')
-            self.bouton_enreg.configure(state='active')
+            self.bouton_lire.configure(state="disabled")
+            self.bouton_stop.configure(state="active")
+            self.bouton_enreg.configure(state="active")
 
     def lire(self):
         self.stop_thread = False
@@ -75,7 +75,7 @@ class PiAudio(Tk):
         t.start()
 
     def _lire(self):
-        with wave.open(self.fichier_courant, 'rb') as w:
+        with wave.open(self.fichier_courant, "rb") as w:
             output = alsaaudio.PCM()
             output.setchannels(w.getnchannels())
             output.setrate(w.getframerate())
@@ -90,13 +90,13 @@ class PiAudio(Tk):
 
     def stop(self):
         self.stop_thread = True
-        self.bouton_lire.configure(state='active')
-        self.bouton_enreg.configure(state='active')
+        self.bouton_lire.configure(state="active")
+        self.bouton_enreg.configure(state="active")
 
     def enregistrer(self):
         self.stop_thread = False
-        self.bouton_lire.configure(state='disabled')
-        self.bouton_enreg.configure(state='disabled')
+        self.bouton_lire.configure(state="disabled")
+        self.bouton_enreg.configure(state="disabled")
         t = threading.Thread(target=self._enregistrer)
         t.start()
 
@@ -107,7 +107,7 @@ class PiAudio(Tk):
         entree.setrate(44100)
         entree.setformat(alsaaudio.PCM_FORMAT_S16_LE)
         entree.setperiodsize(1024)
-        with wave.open(self.fichier_courant, 'w') as w:
+        with wave.open(self.fichier_courant, "w") as w:
             w.setnchannels(1)
             w.setsampwidth(2)
             w.setframerate(44100)
@@ -118,5 +118,5 @@ class PiAudio(Tk):
                     break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     PiAudio()
